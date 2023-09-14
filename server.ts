1517685531470c2f0f998467341c4870312cb16f
modecode22 <epiccoder00@gmail.com>
2023-09-14 13:47:28 +0100

    thats cool I love deno

--- server.ts
+++ server.ts
@@ -1,57 +1,38 @@
-// Importing required modules
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import {
+  Application,
+  Router,
+  SmtpClient,
+  z,
+  oakCors,
+} from "./deps.ts";
+
+
+
+
+
+
+
 
 const router = new Router();
 
-// Defining multiple API endpoints
-
-// GET endpoint
-router.get("/", ({ response }: { response: any }) => {
-  response.body = "Welcome to the home page!";
+router.get("/", async (context) => {
+  try {
+ const lolo = await Math.round(Math.random() * 1000000);
+    context.response.status = 200;
+    context.response.body = { status: "Success", message: `Hello man ${lolo}` };
+  } catch (error) {
+      // Email sending failed
+      console.error(`Email send failed: ${error}`);
+      context.response.status = 500;
+      context.response.body = {
+        status: "Error",
+        message: "somthing wrong man",
+      }}
 });
 
-// POST endpoint
-router.post(
-  "/api",
-  async ({ request, response }: { request: any; response: any }) => {
-    const body = await request.body();
-    response.body = `Request received with data ${JSON.stringify(body.value)}`;
-  }
-);
-
-// PUT endpoint
-router.put(
-  "/api/:id",
-  async ({
-    params,
-    request,
-    response,
-  }: {
-    params: { id: string };
-    request: any;
-    response: any;
-  }) => {
-    const body = await request.body();
-    response.body = `Request received for id ${
-      params.id
-    } with data ${JSON.stringify(body.value)}`;
-  }
-);
-
-// DELETE endpoint
-router.delete(
-  "/api/:id",
-  ({ params, response }: { params: { id: string }; response: any }) => {
-    response.body = `Request received to delete id ${params.id}`;
-  }
-);
-
 const app = new Application();
-
-// Using the router
+app.use(oakCors({origin: "https://moncef.online"}));// cors operation middlware
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-// Listening on port 8000
-console.log("Server is running on port 8000");
-await app.listen({ port: 8000 });
+await app.listen({ port: 3000 });
